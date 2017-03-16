e1b1ae630e3993b1a1df1286a352805e9cbf6849
Joseph Pena <josephpendev@gmail.com>
2017-03-16 18:06:39 -0400

    added directory

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Joe on 3/15/17.
+ */
+public class ExampleClass {
+
+  private int myNumber;
+
+  public ExampleClass(int number) {
+    myNumber = number;
+  }
+
+  public int getMyNumber() {
+    return myNumber;
+  }
+
+}
