cdf78af5926de587569b3b9a71247908e26eb6b8
Joseph Pena <josephpendev@gmail.com>
2017-03-19 17:18:12 -0400

    created Tile class and refactored terrainTypes

--- src/main/java/Hex.java
+++ src/main/java/Hex.java
@@ -1,21 +1,16 @@
 import javafx.util.Pair;
-
 /**
  * Created by Max on 3/16/17.
  */
 public class Hex {
 
-    public int tileNumber;
-    public terrainType terrain;
+    public int tileId;
+    public Terrain.terrainType terrain;
     public int level;
     public Pair location;
 
-    public enum terrainType {
-        Grassland, Jungle, Lake, Rocky, Volcano;
-    }
-
-    public Hex(int tileNumber, terrainType terrain) {
-        this.tileNumber = tileNumber;
+    public Hex(int tileId, Terrain.terrainType terrain) {
+        this.tileId = tileId;
         this.terrain = terrain;
     }
 
