f459d983ec736282d9e10e6f60ac795cbd69e415
tk2021 <tkaknevicius@ufl.edu>
2017-03-19 19:54:57 -0400

    adjacencyValidation complete, terrainValidation tests complete orientation additional access functions added Board setHex sets hex location field

--- src/main/java/HexValidation.java
+++ src/main/java/HexValidation.java
@@ -7,24 +7,29 @@ public class HexValidation {
 
 
 
-    public void adjacencyValidation(Pair<Integer,Integer> coordinatePair) {
+    public static boolean adjacencyValidation(Pair<Integer,Integer> coordinatePair, Board gameBoard) {
+        Pair<Integer,Integer> upLeft = Orientation.upLeftOf(coordinatePair);
+        Pair<Integer,Integer> upRight = Orientation.upRightOf(coordinatePair);
+        Pair<Integer,Integer> downLeft = Orientation.downLeftOf(coordinatePair);
+        Pair<Integer,Integer> downRight = Orientation.downRightOf(coordinatePair);
+        Pair<Integer,Integer> left = Orientation.rightOf(coordinatePair);
+        Pair<Integer,Integer> right = Orientation.leftOf(coordinatePair);
+
+        return(!isLocationNull(upLeft, gameBoard) || !isLocationNull(upRight, gameBoard) || !isLocationNull(downLeft, gameBoard) ||
+                !isLocationNull(downRight, gameBoard) || !isLocationNull(left, gameBoard) || !isLocationNull(right, gameBoard));
+    }
+
+    public static boolean terrainValidation(Hex hex, Pair<Integer,Integer> coordinatePair, Board gameBoard){
+
+        return false;
+    }
 
 
+    public static boolean isLocationNull(Pair<Integer,Integer> coordinatePair, Board gameBoard){
+        Integer locationX = coordinatePair.getKey();
+        Integer locationY = coordinatePair.getValue();
 
-        Integer anchorHexX = coordinatePair.getKey();
-        Integer anchorHexY = coordinatePair.getValue();
-
-        Integer upleftLocX = anchorHexX + Orientation.UPLEFT.getKey();
-        Integer upleftLocY = anchorHexY + Orientation.UPLEFT.getValue();
-
-
-        //Pair<Integer,Integer> upleftPair = coordinatePair.Orientation.UPLEFT;
-
-
-
-
-
-
+        return(gameBoard.gameBoard[locationX][locationY] == null);
     }
 
 
--- src/main/java/Orientation.java
+++ src/main/java/Orientation.java
@@ -13,4 +13,53 @@ public class Orientation {
     public static final Pair<Integer, Integer> DOWNRIGHT = new Pair(-2,1);
     public static final Pair<Integer, Integer> DOWNLEFT = new Pair(-2,-1);
 
+    public static  Pair<Integer,Integer> addPairs (Pair<Integer,Integer> pair1, Pair<Integer,Integer> pair2){
+        Integer pair1X = pair1.getKey();
+        Integer pair1Y = pair1.getValue();
+
+        Integer pair2X = pair2.getKey();
+        Integer pair2Y = pair2.getValue();
+
+        return new Pair<Integer,Integer> (pair1X + pair2X, pair1Y + pair2Y);
+
+    }
+
+    public static Pair<Integer,Integer> upLeftOf(Pair<Integer,Integer> startCoordinates){
+        Integer resultLocationX = startCoordinates.getKey() + Orientation.UPLEFT.getKey();
+        Integer resultLocationY = startCoordinates.getValue() + Orientation.UPLEFT.getValue();
+
+        return new Pair<Integer,Integer> (resultLocationX, resultLocationY);
+    }
+    public static Pair<Integer,Integer> upRightOf(Pair<Integer,Integer> startCoordinates){
+        Integer resultLocationX = startCoordinates.getKey() + Orientation.UPRIGHT.getKey();
+        Integer resultLocationY = startCoordinates.getValue() + Orientation.UPRIGHT.getValue();
+
+        return new Pair<Integer,Integer> (resultLocationX, resultLocationY);
+    }
+    public static Pair<Integer,Integer> downLeftOf(Pair<Integer,Integer> startCoordinates){
+        Integer resultLocationX = startCoordinates.getKey() + Orientation.DOWNLEFT.getKey();
+        Integer resultLocationY = startCoordinates.getValue() + Orientation.DOWNLEFT.getValue();
+
+        return new Pair<Integer,Integer> (resultLocationX, resultLocationY);
+    }
+    public static Pair<Integer,Integer> downRightOf(Pair<Integer,Integer> startCoordinates){
+        Integer resultLocationX = startCoordinates.getKey() + Orientation.DOWNRIGHT.getKey();
+        Integer resultLocationY = startCoordinates.getValue() + Orientation.DOWNRIGHT.getValue();
+
+        return new Pair<Integer,Integer> (resultLocationX, resultLocationY);
+    }
+    public static Pair<Integer,Integer> leftOf(Pair<Integer,Integer> startCoordinates){
+        Integer resultLocationX = startCoordinates.getKey() + Orientation.LEFT.getKey();
+        Integer resultLocationY = startCoordinates.getValue() + Orientation.LEFT.getValue();
+
+        return new Pair<Integer,Integer> (resultLocationX, resultLocationY);
+    }
+    public static Pair<Integer,Integer> rightOf(Pair<Integer,Integer> startCoordinates){
+        Integer resultLocationX = startCoordinates.getKey() + Orientation.RIGHT.getKey();
+        Integer resultLocationY = startCoordinates.getValue() + Orientation.RIGHT.getValue();
+
+        return new Pair<Integer,Integer> (resultLocationX, resultLocationY);
+    }
+
+
 }
