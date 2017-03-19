216c24fa42c2fa8b6177777059e22a3f1bb9940e
troy-halpin <troyhalpin1@gmail.com>
2017-03-19 16:41:23 -0400

    Made Hex fields private and added getter/setters

--- src/main/java/Hex.java
+++ src/main/java/Hex.java
@@ -5,18 +5,50 @@ import javafx.util.Pair;
  */
 public class Hex {
 
-    public int tileNumber;
-    public terrainType terrain;
-    public int level;
-    public Pair location;
+    private int tileNumber;
+    private terrainType terrain;
+    private int level;
+    private Pair location;
 
     public enum terrainType {
-        Grassland, Jungle, Lake, Rocky, Volcano;
+        Grassland, Jungle, Lake, Rocky, Volcano
     }
 
     public Hex(int tileNumber, terrainType terrain) {
         this.tileNumber = tileNumber;
         this.terrain = terrain;
+    }
+
+    public int getTileNumber() {
+        return tileNumber;
+    }
+
+    public void setTileNumber(int tileNumber) {
+        this.tileNumber = tileNumber;
+    }
+
+    public terrainType getTerrain() {
+        return terrain;
+    }
+
+    public void setTerrain(terrainType terrain) {
+        this.terrain = terrain;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Pair getLocation() {
+        return location;
+    }
+
+    public void setLocation(Pair location) {
+        this.location = location;
     }
 
 }
