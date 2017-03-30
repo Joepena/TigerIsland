5ea953a18970886c8921ddeb53be29abcf12b758
troy-halpin <troyhalpin1@gmail.com>
2017-03-30 17:24:06 -0400

    Added steps for a player's turn in comments in PlayerRunnable class. The methods that perform these actions should be the methods in gameAPI

--- src/main/java/PlayerRunnable.java
+++ src/main/java/PlayerRunnable.java
@@ -8,5 +8,25 @@ public class PlayerRunnable implements Runnable {
         System.out.println("Villager count of " + this.toString() + " is: " + game.getVillagerCount());
         System.out.println("Totoro count of " + this.toString() + " is: " + game.getTotoroCount());
         System.out.println("Tiger count of " + this.toString() + " is: " + game.getTigerCount());
+
+        //Player
+
+        //Receive piece
+
+        //Check for tile placement options
+
+        //Check for nuking options
+
+        //Decide normal place or nuke
+
+        //Check settlements
+
+        //Decide move
+
+        //Build/Expand/Tiger/Totoro
+
+        //End Turn
+
+
     }
 }
