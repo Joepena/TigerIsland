485b2e920524c26ebc52ef67a9a7ab3bdcc4cdcd
Joseph Pena <josephpendev@gmail.com>
2017-03-30 20:35:37 -0400

    config test

---
+++ src/test/java/JUnitRunner.java
@@ -0,0 +1,21 @@
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+/**
+ * Created by Joe on 3/30/17.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  BoardTest.class,
+  GameAPITest.class,
+  OrientationClassTest.class,
+  TilePlacementTests.class,
+  TileTest.class,
+  updateSettlementsTest.class
+})
+
+public class JUnitRunner {
+
+}
