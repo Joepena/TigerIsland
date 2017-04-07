429dd5ce5bde8c0b17e62abaae023460ed9a0eb4
tk2021 <tkaknevicius@gmail.com>
2017-04-07 01:27:06 -0400

    Refactored messageParser class

--- src/test/java/messageParserTest.java
+++ src/test/java/messageParserTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 /**
  * Created by T K Vicious on 4/5/2017.
  */
-public class messageFieldsTest {
-    messageFields mf;
+public class messageParserTest {
+    MessageParser mf;
 
     @Before
     public void setUp() throws Exception {
@@ -19,7 +19,7 @@ public class messageFieldsTest {
 
     @Test
     public void NewChallengeMessageTest () throws Exception {
-        this.mf = new messageFields("NEW CHALLENGE A1$ YOU WILL PLAY 12 MATCHES");
+        this.mf = new MessageParser("NEW CHALLENGE A1$ YOU WILL PLAY 12 MATCHES");
         NewChallengeMessage message = new NewChallengeMessage("NEW CHALLENGE A1$ YOU WILL PLAY 12 MATCHES");
 
         Assert.assertTrue("NewChallengeMessage Generation from server string" , message.equals((NewChallengeMessage)mf.getMessage()));
@@ -28,7 +28,7 @@ public class messageFieldsTest {
 
     @Test
     public void WaitToBeginMessageTest () throws Exception {
-        this.mf = new messageFields("WAIT FOR THE TOURNAMENT TO BEGIN 56^T");
+        this.mf = new MessageParser("WAIT FOR THE TOURNAMENT TO BEGIN 56^T");
         WaitToBeginMessage message = new WaitToBeginMessage("WAIT FOR THE TOURNAMENT TO BEGIN 56^T");
 
         Assert.assertTrue("WaitToBeginMessage Generation from server string" , message.equals((WaitToBeginMessage)mf.getMessage()));
@@ -37,7 +37,7 @@ public class messageFieldsTest {
 
     @Test
     public void BeginRoundMessageTest () throws Exception {
-        this.mf = new messageFields("BEGIN ROUND 8 OF 12");
+        this.mf = new MessageParser("BEGIN ROUND 8 OF 12");
         BeginRoundMessage message = new BeginRoundMessage("BEGIN ROUND 8 OF 12");
 
         Assert.assertTrue("BeginRoundMessage Generation from server string" , message.equals((BeginRoundMessage)mf.getMessage()));
@@ -46,7 +46,7 @@ public class messageFieldsTest {
 
     @Test
     public void EndRoundMessageTest () throws Exception {
-        this.mf = new messageFields("END OF ROUND 99 OF 112");
+        this.mf = new MessageParser("END OF ROUND 99 OF 112");
         EndRoundMessage message = new EndRoundMessage("END OF ROUND 99 OF 112");
 
         Assert.assertTrue("EndRoundMessage Generation from server string" , message.equals((EndRoundMessage)mf.getMessage()));
@@ -55,7 +55,7 @@ public class messageFieldsTest {
 
     @Test
     public void MatchBeginningMessageTest () throws Exception {
-        this.mf = new messageFields("NEW MATCH BEGINNING NOW YOUR OPPONENT IS PLAYER ABC");
+        this.mf = new MessageParser("NEW MATCH BEGINNING NOW YOUR OPPONENT IS PLAYER ABC");
         MatchBeginningMessage message = new MatchBeginningMessage("NEW MATCH BEGINNING NOW YOUR OPPONENT IS PLAYER ABC");
 
         Assert.assertTrue("MatchBeginningMessage Generation from server string" , message.equals((MatchBeginningMessage)mf.getMessage()));
@@ -64,7 +64,7 @@ public class messageFieldsTest {
 
     @Test
     public void MoveFoundSettlementMessageTest () throws Exception {
-        this.mf = new messageFields("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 FOUNDED SETTLEMENT AT 12 23 1");
+        this.mf = new MessageParser("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 FOUNDED SETTLEMENT AT 12 23 1");
         FoundSettlementMessage message = new FoundSettlementMessage("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 FOUNDED SETTLEMENT AT 12 23 1");
 
         Assert.assertTrue("FoundSettlementMessage Generation from server string" , message.equals((FoundSettlementMessage)mf.getMessage()));
@@ -74,7 +74,7 @@ public class messageFieldsTest {
 
     @Test
     public void MoveExpandSettlementMessageTest () throws Exception {
-        this.mf = new messageFields("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 EXPANDED SETTLEMENT AT 65 89 45 JUNGLE");
+        this.mf = new MessageParser("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 EXPANDED SETTLEMENT AT 65 89 45 JUNGLE");
         ExpandSettlementMessage message = new ExpandSettlementMessage("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 EXPANDED SETTLEMENT AT 65 89 45 JUNGLE");
 
         Assert.assertTrue("ExpandSettlementMessage Generation from server string" , message.equals((ExpandSettlementMessage)mf.getMessage()));
@@ -84,7 +84,7 @@ public class messageFieldsTest {
 
     @Test
     public void MoveBuildTotoroMessageTest () throws Exception {
-        this.mf = new messageFields("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 BUILT TOTORO SANCTUARY AT 15 53 44");
+        this.mf = new MessageParser("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 BUILT TOTORO SANCTUARY AT 15 53 44");
         BuildTotoroMessage message = new BuildTotoroMessage("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 BUILT TOTORO SANCTUARY AT 15 53 44");
 
         Assert.assertTrue("BuildTotoroMessage Generation from server string" , message.equals((BuildTotoroMessage)mf.getMessage()));
@@ -95,7 +95,7 @@ public class messageFieldsTest {
 
     @Test
     public void MoveBuildTigerMessageTest () throws Exception {
-        this.mf = new messageFields("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 BUILT TIGER PLAYGROUND AT 15 53 44");
+        this.mf = new MessageParser("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 BUILT TIGER PLAYGROUND AT 15 53 44");
         BuildTigerMessage message = new BuildTigerMessage("GAME 123 MOVE 12 PLAYER DGD PLACED LAKE+ROCK AT 10 -12 11 3 BUILT TIGER PLAYGROUND AT 15 53 44");
 
         Assert.assertTrue("BuildTigerMessage Generation from server string" , message.equals((BuildTigerMessage)mf.getMessage()));
@@ -106,7 +106,7 @@ public class messageFieldsTest {
 
     @Test
     public void ForfeitMessageTest () {
-        this.mf = new messageFields("GAME 123 MOVE 12 PLAYER DGD FORFEITED: ILLEGAL TILE PLACEMENT");
+        this.mf = new MessageParser("GAME 123 MOVE 12 PLAYER DGD FORFEITED: ILLEGAL TILE PLACEMENT");
         ForfeitMessage message = new ForfeitMessage("GAME 123 MOVE 12 PLAYER DGD FORFEITED: ILLEGAL TILE PLACEMENT");
 
         Assert.assertTrue("ForfeitMessage Generation from server string" , message.equals((ForfeitMessage)mf.getMessage()));
@@ -117,7 +117,7 @@ public class messageFieldsTest {
 
     @Test
     public void GameOverMessageTest() {
-        this.mf = new messageFields("GAME 1290 OVER PLAYER ABC 23000 PLAYER DEF 230000");
+        this.mf = new MessageParser("GAME 1290 OVER PLAYER ABC 23000 PLAYER DEF 230000");
         GameOverMessage message = new GameOverMessage("GAME 1290 OVER PLAYER ABC 23000 PLAYER DEF 230000");
 
         Assert.assertTrue("GameOverMessage Generation from server string" , message.equals((GameOverMessage)mf.getMessage()));
@@ -126,7 +126,7 @@ public class messageFieldsTest {
 
     @Test
     public void MakeYourMoveMessageTest() {
-        this.mf = new messageFields("MAKE YOUR MOVE IN GAME 12345 WITHIN 1.545 SECONDS: MOVE 1234 PLACE ROCK+LAKE");
+        this.mf = new MessageParser("MAKE YOUR MOVE IN GAME 12345 WITHIN 1.545 SECONDS: MOVE 1234 PLACE ROCK+LAKE");
         MakeYourMoveMessage message = new MakeYourMoveMessage("MAKE YOUR MOVE IN GAME 12345 WITHIN 1.545 SECONDS: MOVE 1234 PLACE ROCK+LAKE");
 
         Assert.assertTrue("MakeYourMoveMessage Generation from server string" , message.equals((MakeYourMoveMessage)mf.getMessage()));
@@ -136,19 +136,19 @@ public class messageFieldsTest {
 
     @Test
     public void noActionMessageTest () throws Exception {
-        this.mf = new messageFields("WELCOME TO ANOTHER EDITION OF THUNDERDOME!");
+        this.mf = new MessageParser("WELCOME TO ANOTHER EDITION OF THUNDERDOME!");
         Assert.assertTrue("Welcome message sets MessageFields type field correctly", mf.getMessage().getMessageType() == Message.MessageType.Welcome);
 
-        this.mf = new messageFields("TWO SHALL ENTER ONE SHALL LEAVE");
+        this.mf = new MessageParser("TWO SHALL ENTER ONE SHALL LEAVE");
         Assert.assertTrue("Welcome message sets MessageFields type field correctly", mf.getMessage().getMessageType() == Message.MessageType.Enter);
 
-        this.mf = new messageFields("END OF CHALLENGES");
+        this.mf = new MessageParser("END OF CHALLENGES");
         Assert.assertTrue("Welcome message sets MessageFields type field correctly", mf.getMessage().getMessageType() == Message.MessageType.EndOfChallenges);
 
-        this.mf = new messageFields("WAIT FOR THE NEXT CHALLENGE TO BEGIN");
+        this.mf = new MessageParser("WAIT FOR THE NEXT CHALLENGE TO BEGIN");
         Assert.assertTrue("Welcome message sets MessageFields type field correctly", mf.getMessage().getMessageType() == Message.MessageType.WaitForNext);
 
-        this.mf = new messageFields("THANK YOU FOR PLAYING! GOODBYE");
+        this.mf = new MessageParser("THANK YOU FOR PLAYING! GOODBYE");
         Assert.assertTrue("Welcome message sets MessageFields type field correctly", mf.getMessage().getMessageType() == Message.MessageType.Goodbye);
     }
 
@@ -175,7 +175,7 @@ public class messageFieldsTest {
     /*
     @Test
     public void parsePlacementMessageTest() {
-        messageFields m = new messageFields("GAME 5 MOVE 4 PLAYER 23 PLACED LAKE+ROCKY AT 3 5 4 6");
+        MessageParser m = new MessageParser("GAME 5 MOVE 4 PLAYER 23 PLACED LAKE+ROCKY AT 3 5 4 6");
         System.out.println(m.toString());
     }*/
 
