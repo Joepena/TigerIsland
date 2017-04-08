e97aa953aaf28952e16e12df163b55b8c4a559da
Joe Pena <Joepena@users.noreply.github.com>
2017-04-08 14:35:35 -0400

    Update NoActionMessage.java

--- src/main/MessageTypes/NoActionMessage.java
+++ src/main/MessageTypes/NoActionMessage.java
@@ -2,7 +2,7 @@
  * Created by TomasK on 4/6/2017.
  */
 public class NoActionMessage extends Message {
-    noActionMessage(Message.MessageType messageType){
+    NoActionMessage(Message.MessageType messageType){
         super(messageType);
     }
 }
