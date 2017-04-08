ae075c75f045f88456f6278032cc6264ad09b0f0
Nicholas <nbarnes@hotmail.com>
2017-04-08 15:55:32 -0400

    begun implementing AI Logic

--- src/main/java/PlayerRunnable.java
+++ src/main/java/PlayerRunnable.java
@@ -20,6 +20,15 @@ public class PlayerRunnable implements Runnable {
         System.out.println("Totoro count of " + this.toString() + " is: " + game.getTotoroCount());
         System.out.println("Tiger count of " + this.toString() + " is: " + game.getTigerCount());
 
+        //Instantiate all ArrayLists once
+        ArrayList<Tuple> tilePlacementOptions = new ArrayList<>();
+        ArrayList<Tuple> eruptionOptions = new ArrayList<>();
+        ArrayList<Tuple> foundSettlementOptions = new ArrayList<>();
+        ArrayList<ExpansionOpDataFrame> expandSettlementOptions = new ArrayList<>();
+        ArrayList<Tuple> totoroPlacementOptions = new ArrayList<>();
+        ArrayList<Tuple> tigerPlacementOptions = new ArrayList<>();
+
+
         //Player Logic
 
         game.placeFirstTile();
@@ -32,24 +41,36 @@ public class PlayerRunnable implements Runnable {
             game.updateSettlements();
 
             //Check for tile placement options
-
+            tilePlacementOptions = game.getAvailableTilePlacement();
 
             //Check for nuking options
-            ArrayList<Tuple> eruptionOptions = new ArrayList<>();
             eruptionOptions = game.getValidNukingLocations();
 
             //Decide normal place or nuke
 
 
+
             //Place tile
             game.placeTile(newTile, decisionCoords);
 
-            //Check settlements
+            //Update board state
             game.updateSettlements();
 
-            //Decide move
+            //Check for Found Settlement options
+            foundSettlementOptions = game.findListOfValidSettlementLocations();
 
-            //Build/Expand/Tiger/Totoro
+            //Check for Expand Settlement options
+            expandSettlementOptions = game.getExpansionOptions(Hex.Team.Black);
+
+            //Check for Totoro placement options
+            totoroPlacementOptions = game.validTotoroPlacements();
+
+            //Check for Tiger placement options
+            tigerPlacementOptions = game.validTigerPlacements();
+
+            //Decide Build Action
+
+            //Perform Build action
 
         }
     }
