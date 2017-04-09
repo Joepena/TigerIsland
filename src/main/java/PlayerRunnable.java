991f227238fd0c0dea109e393091de6b5ecea2d3
Nicholas <nbarnes@hotmail.com>
2017-04-09 19:17:16 -0400

    Added AI shells for both strategies

--- src/main/java/PlayerRunnable.java
+++ src/main/java/PlayerRunnable.java
@@ -28,7 +28,7 @@ public class PlayerRunnable implements Runnable {
         return game;
     }
 
-    public PlayerRunnable (String playerID, String opponentID){
+    public PlayerRunnable(String playerID, String opponentID) {
         this.playerID = playerID;
         this.opponentID = opponentID;
         this.gameID = "";
@@ -49,7 +49,7 @@ public class PlayerRunnable implements Runnable {
 
     @Override
     public void run() {
-       // System.out.println("Villager count of " + this.toString() + " is: " + game.getVillagerCount());
+        // System.out.println("Villager count of " + this.toString() + " is: " + game.getVillagerCount());
         //System.out.println("Totoro count of " + this.toString() + " is: " + game.getTotoroCount());
         //System.out.println("Tiger count of " + this.toString() + " is: " + game.getTigerCount());
 
@@ -66,8 +66,8 @@ public class PlayerRunnable implements Runnable {
 
         game.placeFirstTile();
 
-        while(!gameOver) {
-            while(!hasMove);
+        while (!gameOver) {
+            while (!hasMove) ;
             this.moveMessage = new clientMoveMessages();
 
             //Update board state
@@ -80,140 +80,193 @@ public class PlayerRunnable implements Runnable {
             eruptionOptions = game.getValidNukingLocations();
 
             //Decide normal place or nuke
-            if (canNukeSafely()) {
-                if (canSabotageEnemySettlement()){
-
-                } else if (canRecycleOwnTotoroSettlement()){
-
+            //Tiger Rush Strategy
+            if (canNukeSafely(game)) {
+                if (canSabotageEnemySettlement()) {
+                    //sabotage enemy settlement
+                } else if (canRecycleOwnTotoroSettlement()) {
+                    //recycle own Totoro settlement
                 } else {
-                    //Pick a random nuking location.
+                    //nuke tile nearest origin.
+                }
+            } else {
+                //Place tile nearest origin.
+            }
+
+            //Totoro Turtle Strategy
+            if (totoroPiecesRemaining(game)) {
+                if (canNukeSafely(game)) {
+                    if (canSabotageEnemySettlement()) {
+                        //Sabotage enemy settlement
+                    }
+                } else {
+                    //Place tiles near existing settlements of < 5 w/ no totoro
                 }
             } else{
-                //Pick a random tile placement location.
+                if (canNukeSafely(game)) {
+                    //Nuke tiles near existing settlements (to prep for expansion)
+                } else {
+                    //Place tile
+                }
             }
 
-            moveMessage.setTileLocation(tilePlacementOptions.get(0));
-            Orientation.Orientations orientation = game.APIUtils.getViableNonNukingOrientation(tilePlacementOptions.get(0));
-            //int orientation = game.APIUtils.findValidOrientationsAtPoint(tilePlacementOptions.get(0)).get(0);
-            moveMessage.setOrientation(moveMessage.orientationToNumber(orientation));
-            //moveMessage.setOrientation(orientation);
-            moveMessage.setTile(newTile);
-            moveMessage.setGid(this.gameID);
-            moveMessage.setMoveNumber(this.moveNumber);
+                moveMessage.setTileLocation(tilePlacementOptions.get(0));
+                Orientation.Orientations orientation = game.APIUtils.getViableNonNukingOrientation(tilePlacementOptions.get(0));
+                //int orientation = game.APIUtils.findValidOrientationsAtPoint(tilePlacementOptions.get(0)).get(0);
+                moveMessage.setOrientation(moveMessage.orientationToNumber(orientation));
+                //moveMessage.setOrientation(orientation);
+                moveMessage.setTile(newTile);
+                moveMessage.setGid(this.gameID);
+                moveMessage.setMoveNumber(this.moveNumber);
 
 
-            //Place tile to handle on our own board
-            newTile.setLeftHexOrientation(orientation);
-            game.placeTile(newTile, tilePlacementOptions.get(0));
+                //Place tile to handle on our own board
+                newTile.setLeftHexOrientation(orientation);
+                game.placeTile(newTile, tilePlacementOptions.get(0));
 
-            //Update board state
-            game.updateSettlements();
+                //Update board state
+                game.updateSettlements();
 
-            //Check for Found Settlement options
-            foundSettlementOptions = game.findListOfValidSettlementLocations();
+                //Check for Found Settlement options
+                foundSettlementOptions = game.findListOfValidSettlementLocations();
 
-            //Check for Expand Settlement options
-            expandSettlementOptions = game.getExpansionOptions(Hex.Team.Black);
+                //Check for Expand Settlement options
+                expandSettlementOptions = game.getExpansionOptions(Hex.Team.Black);
 
-            //Check for Totoro placement options
-            totoroPlacementOptions = game.validTotoroPlacements();
+                //Check for Totoro placement options
+                totoroPlacementOptions = game.validTotoroPlacements();
 
-            //Check for Tiger placement options
-            tigerPlacementOptions = game.validTigerPlacements();
+                //Check for Tiger placement options
+                tigerPlacementOptions = game.validTigerPlacements();
 
-            //Decide Build Action
-            if (canPlaceTiger(tigerPlacementOptions)){
-                buildDecision = new BuildDecision(tigerPlacementOptions.get(0), BuildDecision.buildDecisions.Tiger);
-            } else if (tigerPiecesRemaining(game)){
-                buildDecision = new BuildDecision(foundSettlementOptions.get(0), BuildDecision.buildDecisions.Found);
-            } else if (canExpand(expandSettlementOptions)){
-                buildDecision = new BuildDecision(expandSettlementOptions.get(0).getExpansionStart(), BuildDecision.buildDecisions.Expand);
-            }else if (canPlaceTotoro(totoroPlacementOptions)){
-                buildDecision = new BuildDecision(totoroPlacementOptions.get(0), BuildDecision.buildDecisions.Totoro);
-            } else
-            {
-                buildDecision = new BuildDecision(foundSettlementOptions.get(0), BuildDecision.buildDecisions.Found);
+                //Decide Build Action
+                //Tiger Rush Strategy
+                if (tigerPiecesRemaining(game)) {
+                    if (canPlaceTiger(tigerPlacementOptions)) {
+                        //place Tiger
+                    } else if (canPlaceTotoro(totoroPlacementOptions)) {
+                        //place Totoro
+                    } else {
+                        //found settlement
+                    }
+                } else {
+                    if (canExpand(expandSettlementOptions)) {
+                        //choose highest cost expansion option
+                    } else {
+                        //found settlement
+                    }
+                }
+
+                //Totoro Turtle Strategy
+                if (totoroPiecesRemaining(game)) {
+                    if (canPlaceTotoro(totoroPlacementOptions)) {
+                        //place Totoro
+                    } else if (canPlaceTiger(tigerPlacementOptions)) {
+                        //place Tiger
+                    } else if (existsSub3ExpandOption()) {
+                        //perform expansion with cost of 1-2
+                    } else {
+                        //found settlement near largest settlement w/ no totoro
+                    }
+                } else {
+                    if (canExpand(expandSettlementOptions)) {
+                        //choose highest-cost expansion option
+                    } else {
+                        //Found settlement
+                    }
+                }
+
+
+                //FOR SERVER TESTING
+                moveMessage.setBuildLocation(foundSettlementOptions.get(0));
+                moveMessage.setMoveType(clientMoveMessages.clientMoveMessageType.Found);
+
+                //Perform Build action
+                //game.foundSettlement(buildDecisionCoords, Hex.Team.Black);
+
+                //Print out moveMessage and send to Client
+
+                System.out.println(moveMessage.toString(moveMessage.getMoveType()));
+
+                hasMove = false;
+                gameOver = true;
             }
-
-
-            //FOR SERVER TESTING
-            moveMessage.setBuildLocation(foundSettlementOptions.get(0));
-            moveMessage.setMoveType(clientMoveMessages.clientMoveMessageType.Found);
-
-            //Perform Build action
-            //game.foundSettlement(buildDecisionCoords, Hex.Team.Black);
-
-            //Print out moveMessage and send to Client
-
-            System.out.println(moveMessage.toString(moveMessage.getMoveType()));
-
-            hasMove = false;
-            gameOver = true;
         }
+
+    private boolean existsSub3ExpandOption() {
+        return false;
     }
 
-
-    private boolean canNukeSafely() {
-      return false;
+    private boolean canNukeSafely(GameAPI game) {
+        return game.findListOfValidSettlementLocations().size() > 2;
     }
+
     private boolean canSabotageEnemySettlement() {
         return false;
     }
+
     private boolean canRecycleOwnTotoroSettlement() {
         return false;
     }
+
     private boolean canPlaceTiger(ArrayList<Tuple> tigerOptions) {
-      return !tigerOptions.isEmpty();
+        return !tigerOptions.isEmpty();
     }
-    private boolean tigerPiecesRemaining(GameAPI game) {
-      return game.getTigerCount() > 0;
-    }
-    private boolean canExpand(ArrayList<ExpansionOpDataFrame> expandSettlementOptions) {
-      return !expandSettlementOptions.isEmpty();
-    }
-    private boolean canPlaceTotoro(ArrayList<Tuple> totoroPlacementOptions) {
-      return !totoroPlacementOptions.isEmpty();
+    private boolean totoroPiecesRemaining(GameAPI game) {
+        return game.getTotoroCount() > 0;
     }
 
-    public void executeMessage(Message message){
+    private boolean tigerPiecesRemaining(GameAPI game) {
+        return game.getTigerCount() > 0;
+    }
+
+    private boolean canExpand(ArrayList<ExpansionOpDataFrame> expandSettlementOptions) {
+        return !expandSettlementOptions.isEmpty();
+    }
+
+    private boolean canPlaceTotoro(ArrayList<Tuple> totoroPlacementOptions) {
+        return !totoroPlacementOptions.isEmpty();
+    }
+
+    public void executeMessage(Message message) {
 
         Message.MessageType type = message.getMessageType();
 
-        switch(type){
+        switch (type) {
             case GameOver:
-                GameOverMessage gameOverMessage = (GameOverMessage)message;
-                if(!gameOverMessage.getGid().equals(this.gameID))
+                GameOverMessage gameOverMessage = (GameOverMessage) message;
+                if (!gameOverMessage.getGid().equals(this.gameID))
                     return;
                 GameOver(gameOverMessage);
                 break;
             case MakeYourMove:
                 MakeYourMoveMessage makeYourMove = (MakeYourMoveMessage) message;
-                if(!makeYourMove.getGid().equals(this.gameID))
+                if (!makeYourMove.getGid().equals(this.gameID))
                     return;
                 MakeYourMove(makeYourMove);
                 break;
             case Move:
                 MoveMessage move = (MoveMessage) message;
-                if(!move.getGid().equals(this.gameID))
+                if (!move.getGid().equals(this.gameID))
                     return;
                 Move(move);
                 break;
         }
     }
 
-    private void GameOver(GameOverMessage message){
+    private void GameOver(GameOverMessage message) {
         this.gameOver = true;
     }
 
-    private void MakeYourMove(MakeYourMoveMessage message){
+    private void MakeYourMove(MakeYourMoveMessage message) {
         this.newTile = message.getTile();
         this.hasMove = true;
         this.moveNumber = message.getMoveNumber();
     }
 
 
-    private void Move(MoveMessage message){
-        switch(message.getMoveType()){
+    private void Move(MoveMessage message) {
+        switch (message.getMoveType()) {
             case Found:
                 FoundMove((FoundSettlementMessage) message);
                 break;
@@ -232,34 +285,34 @@ public class PlayerRunnable implements Runnable {
         }
     }
 
-    private void FoundMove(FoundSettlementMessage message){
-        if(message.getPid().equals(this.playerID))
+    private void FoundMove(FoundSettlementMessage message) {
+        if (message.getPid().equals(this.playerID))
             return;
         game.placeTile(message.getTile(), message.getTileLocation());
         game.foundSettlement(message.getBuildLocation(), this.opponentTeam);
     }
 
-    private void Expand(ExpandSettlementMessage message){
-        if(message.getPid().equals(this.playerID))
+    private void Expand(ExpandSettlementMessage message) {
+        if (message.getPid().equals(this.playerID))
             return;
         game.placeTile(message.getTile(), message.getTileLocation());
         //game.expandSettlement();
 
     }
 
-    private void Forfeit(ForfeitMessage message){
+    private void Forfeit(ForfeitMessage message) {
         return;
     }
 
-    private void Tiger(BuildTigerMessage message){
-        if(message.getPid().equals(this.playerID))
+    private void Tiger(BuildTigerMessage message) {
+        if (message.getPid().equals(this.playerID))
             return;
         game.placeTile(message.getTile(), message.getTileLocation());
         game.createTigerPlayground(message.getBuildLocation(), this.opponentTeam);
     }
 
-    private void Totoro(BuildTotoroMessage message){
-        if(message.getPid().equals(this.playerID))
+    private void Totoro(BuildTotoroMessage message) {
+        if (message.getPid().equals(this.playerID))
             return;
         game.placeTile(message.getTile(), message.getTileLocation());
         game.createTotoroSanctuary(message.getBuildLocation(), this.opponentTeam);
@@ -269,7 +322,7 @@ public class PlayerRunnable implements Runnable {
         this.gameOver = gameOver;
     }
 
-    public String toString(){
+    public String toString() {
         String string = ("playerID:  " + this.playerID + "\nopponentID:  " + this.opponentID + "\ngameID:  " + this.gameID);
         return string;
     }
